43f8db3e21604fba7867cb7741e228d795872cc7
Jorge Ferreiro <me@jgferreiro.com>
2016-06-04 00:41:24 +0200

    Added Projects and Moved Code to Theory

---
+++ Projects/MyTodoList/MyTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by JF on 04/06/16.
+//  Copyright © 2016 Jorge Ferreiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
