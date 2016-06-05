947b31458cb1a19944db2d5f8dbc5a28ba88c49f
Jorge Ferreiro <me@jgferreiro.com>
2016-06-06 01:27:12 +0200

    Moved Assets to root

---
+++ Projects/MyTodoList/MyTodoList/TodoList.swift
@@ -0,0 +1,13 @@
+//
+//  TodoList.swift
+//  MyTodoList
+//
+//  Created by JF on 06/06/16.
+//  Copyright © 2016 Jorge Ferreiro. All rights reserved.
+//
+
+import UIKit
+
+class TodoList: NSObject {
+
+}
